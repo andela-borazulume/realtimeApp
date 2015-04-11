5eefd744b2108a03ea8f1c593cb003c418b157ff
Andela Developer <blessing.orazulume@andela.co>
2015-04-11 09:33:39 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name thinksterFanApp
+ * @description
+ * # thinksterFanApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('thinksterFanApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
