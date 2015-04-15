c526d15f93140d945c59f4c6e8fb28294ca8b82b
Andela Developer <blessing.orazulume@andela.co>
2015-04-15 10:23:15 +0100

    testing this

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -2,7 +2,7 @@
 
 app.controller('NavCtrl', function($scope, $location, Post){
 	$scope.post = {url:'http://', title: '' };
-	
+
 	$scope.submitPost = function(){
 		Post.create($scope.post).then(function(ref){
 			$location.path('/posts/'+ ref.name());
--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('PostsCtrl', function ($scope, Post, $location) {
+app.controller('PostsCtrl', function ($scope, Post) {
 	$scope.posts = Post.all;
 	$scope.post = {url:'http://', title: '' };
 	
